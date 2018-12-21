6ed882cb7a6806a15bfbb8627c97fee16cb6b691
yufeng <yufeng0125@gmail.com>
2018-12-21 22:35:23 +0800

    使用RecyclerView的方式进行

--- app/src/main/java/com/yefoo/demowebviwe/ExploreFrameLayout.java
+++ app/src/main/java/com/yefoo/demowebviwe/ExploreFrameLayout.java
@@ -28,11 +28,6 @@ import android.widget.Scroller;
 public class ExploreFrameLayout extends FrameLayout implements NestedScrollingParent {
     private static final String TAG = ExploreFrameLayout.class.getSimpleName();
     private int limitHeight = 0;
-    private float scrollY = 0;
-    private float beginY = 0;
-    private BaseWebView baseWebView;
-
-    private float initY, initScrollY;
 
     private VelocityTracker mVelocityTracker;
     private int mMaximumVelocity, mMinimumVelocity;
@@ -65,7 +60,6 @@ public class ExploreFrameLayout extends FrameLayout implements NestedScrollingPa
 
     private void init(Context context) {
         limitHeight = dip2px(context, 200);
-        baseWebView = getBaseWebView();
 
         mScroller = new Scroller(context);
         mVelocityTracker = VelocityTracker.obtain();
@@ -74,112 +68,6 @@ public class ExploreFrameLayout extends FrameLayout implements NestedScrollingPa
         mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
     }
 
-    private BaseWebView getBaseWebView() {
-        int childCount = getChildCount();
-        if (childCount >= 2) {
-            View view = getChildAt(1);
-            return (BaseWebView) view;
-        }
-        return null;
-    }
-
-//    @Override
-//    public boolean onTouchEvent(MotionEvent event) {
-//        initVelocityTrackerIfNotExists();
-//        switch (event.getAction()) {
-//            case MotionEvent.ACTION_DOWN:
-//                mVelocityTracker.clear();
-//                mVelocityTracker.addMovement(event);
-//                break;
-//            case MotionEvent.ACTION_MOVE:
-//
-//                float deltaY = event.getY() - initY - initScrollY;
-//
-////                scrollY += (curY - beginY);
-////                beginY = curY;
-//
-//                if (deltaY < 0) { //向上滚动
-//
-//                    if (-deltaY < limitHeight) {
-//                        scrollTo(0, (int) -deltaY);
-//                        return true;
-//                    } else { //需要手动分发down事件
-//                        scrollTo(0, limitHeight);
-//                        initY = event.getY();
-//                        initScrollY = getScrollY();
-//
-//                        if (getScrollY() == limitHeight) {
-//                            event.setAction(MotionEvent.ACTION_DOWN);
-//                            dispatchTouchEvent(event);
-//                        }
-//                    }
-//
-//
-//                } else if (deltaY > 0) {//向下滚动
-//
-//                    if (getScrollY() > 0) {
-//                        scrollTo(0, (int) (initScrollY - (event.getY() - initY)));
-//                        return true;
-//                    } else {
-//                        scrollTo(0, 0);
-//                        initY = event.getY();
-//                        initScrollY = getScrollY();
-//                    }
-//
-//                }
-//                break;
-//            case MotionEvent.ACTION_UP:
-//                beginY = 0;
-//                initY = event.getY();
-//                performClick();
-//                mVelocityTracker.computeCurrentVelocity(1000, mMaximumVelocity);
-//                int velocityY = (int) mVelocityTracker.getYVelocity();
-//                if (Math.abs(velocityY) > mMinimumVelocity) {
-//                    fling(-velocityY);
-//                }
-//                recycleVelocityTracker();
-//                break;
-//            case MotionEvent.ACTION_CANCEL:
-//                recycleVelocityTracker();
-//                if (!mScroller.isFinished()) {
-//                    mScroller.abortAnimation();
-//                }
-//                break;
-//        }
-//        return super.onTouchEvent(event);
-//    }
-
-    @Override
-    public boolean performClick() {
-        return super.performClick();
-    }
-
-//    @Override
-//    public boolean dispatchTouchEvent(MotionEvent ev) {
-//        int action = ev.getAction();
-//        float y = ev.getY();
-//
-//        switch (action) {
-//            case MotionEvent.ACTION_DOWN:
-//                initY = y;
-//                break;
-//            case MotionEvent.ACTION_MOVE:
-//                float dy = y - initY;
-//
-//                BaseWebView baseWebView = getBaseWebView();
-//                if (baseWebView != null && baseWebView.isTop() && dy > 0 && !isInControl) {
-//                    isInControl = true;
-//                    ev.setAction(MotionEvent.ACTION_CANCEL);
-//                    MotionEvent ev2 = MotionEvent.obtain(ev);
-//                    dispatchTouchEvent(ev);
-//                    ev2.setAction(MotionEvent.ACTION_DOWN);
-//                    return dispatchTouchEvent(ev2);
-//                }
-//
-//                break;
-//        }
-//        return super.dispatchTouchEvent(ev);
-//    }
 
     public void fling(int velocityY) {
         mScroller.fling(0, getScrollY(), 0, velocityY, 0, 0, 0, limitHeight);
@@ -208,53 +96,6 @@ public class ExploreFrameLayout extends FrameLayout implements NestedScrollingPa
         }
     }
 
-
-//    @Override
-//    public boolean onInterceptTouchEvent(MotionEvent ev) {
-//        if (baseWebView == null) {
-//            baseWebView = getBaseWebView();
-//        }
-//        switch (ev.getAction()) {
-//            case MotionEvent.ACTION_DOWN:
-//
-//                initY = ev.getY();
-//                initScrollY = getScrollY();
-//                break;
-//            case MotionEvent.ACTION_MOVE:
-//                return isShouldIntercept(ev);
-//            case MotionEvent.ACTION_UP:
-//                initY = 0;
-//                break;
-//        }
-//        return super.onInterceptTouchEvent(ev);
-//    }
-
-//    private boolean isShouldIntercept(MotionEvent ev) {
-//        if (baseWebView == null) {
-//            baseWebView = getBaseWebView();
-//        }
-//        if (Math.abs(ev.getY() - initY) <= mTouchSlop) {
-//            return super.onInterceptTouchEvent(ev);
-//        }
-//        boolean isPullDown = false;
-//        if (ev.getY() - initY > 0) {
-//            isPullDown = true;
-//        } else if (ev.getY() - initY < 0) {
-//            isPullDown = false;
-//        }
-//
-//        if ((!isPullDown && getScrollY() < limitHeight) || (isPullDown && getScrollY() > 0 && baseWebView != null && baseWebView.isTop())) {
-//            if (isPullDown && getScrollY() > 0 && baseWebView != null && baseWebView.isTop()) {
-//                initY = ev.getY();
-//            }
-//            Log.i(TAG, "onInterceptTouchEvent - >拦截");
-//            initVelocityTrackerIfNotExists();
-//            mVelocityTracker.addMovement(ev);
-//            return true;
-//        }
-//
-//        return super.onInterceptTouchEvent(ev);
-//    }
 
     private void recycleVelocityTracker() {
         if (mVelocityTracker != null) {
--- app/src/main/java/com/yefoo/demowebviwe/MainActivity.java
+++ app/src/main/java/com/yefoo/demowebviwe/MainActivity.java
@@ -3,21 +3,34 @@ package com.yefoo.demowebviwe;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebView;
 import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView webView = findViewById(R.id.webView);
-        webView.loadUrl("http://www.baidu.com");
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        myAdapter = new MyAdapter();
+        recyclerView.setAdapter(myAdapter);
 
-        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) webView.getLayoutParams();
+        FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) recyclerView.getLayoutParams();
         layoutParams.height = getResources().getDisplayMetrics().heightPixels - getStateBar();
-        webView.setLayoutParams(layoutParams);
+        recyclerView.setLayoutParams(layoutParams);
     }
 
     private int getStateBar() {
@@ -30,12 +43,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public static int getDaoHangHeight(Context context) {
-        int resourceId ;
+        int resourceId;
         int rid = context.getResources().getIdentifier("config_showNavigationBar", "bool", "android");
         if (rid != 0) {
             resourceId = context.getResources().getIdentifier("navigation_bar_height", "dimen", "android");
             return context.getResources().getDimensionPixelSize(resourceId);
         } else
             return 0;
+    }
+
+    private class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+        private List<String> data = new ArrayList<>();
+
+        private MyAdapter() {
+            for (int i = 0; i < 100; i++) {
+                data.add(String.valueOf(i));
+            }
+        }
+
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recycler, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(MyViewHolder holder, int position) {
+            holder.numTv.setText(data.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+            private TextView numTv;
+
+            private MyViewHolder(View view) {
+                super(view);
+                numTv = view.findViewById(R.id.item_num);
+            }
+        }
     }
 }
